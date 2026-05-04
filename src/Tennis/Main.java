6809f1d7e1cb7883a997e5d8b71c22bd1ad8321f
JaimeLoPz <168116631+JaimeLoPz@users.noreply.github.com>
2026-05-04 23:08:34 +0200

    Change welcome message to Spanish
    cambio en main

--- src/Tennis/Main.java
+++ src/Tennis/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome TO TENNIS GAME!"+"\n");
+        System.out.printf("Bienvenido al juego de tenis de Jaime López Prieto"+"\n");
 
         TennisGame partido1 = new TennisGame1("antonio", "jose");
         partido1.wonPoint("antonio");
